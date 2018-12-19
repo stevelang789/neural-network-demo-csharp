9124de438a65cd5234e5ecfcfbf8b010b22da676
Steve Lang <steve.lang@icarehealth.com>
2018-12-19 14:59:09 +1100

    Refactor Recognizer.cs and update README.md

--- HandwrittenDigitRecognition/Recognizer.cs
+++ HandwrittenDigitRecognition/Recognizer.cs
@@ -10,14 +10,18 @@ namespace HandwrittenDigitRecognition
     {
         public async Task Run(Func<UIElement, int> attachToUi)
         {
-            const int neuronsInInputLayer = Visualizer.ImageWidthHeight * Visualizer.ImageWidthHeight;
-            const int neuronsInOutputLayer = 10;
+            var activator = new LogisticSigmoidActivator(); // ReluActivator seems to be the best one
+            const int numberOfNeuronsInInputLayer = Visualizer.ImageWidthHeight * Visualizer.ImageWidthHeight;
+            const int numberOfNeuronsInHiddenLayer = 10; // Increase this to 30 for best results
+            const int numberOfNeuronsInOutputLayer = 10;
+            const int numberOfEpochs = 5; // More than 5 doesn't seem to make any difference
 
             var network = new Network(
-                new ReluActivator(),
-                neuronsInInputLayer,
-                10,
-                neuronsInOutputLayer);
+                activator,
+                numberOfNeuronsInInputLayer,
+                numberOfNeuronsInHiddenLayer,
+                // Optionally add more hidden layers by specifying the number of neurons in each layer: 10, 10,
+                numberOfNeuronsInOutputLayer);
 
             var trainer = new Trainer(network);
             var panel = Visualizer.NeuralNetRenderer(trainer, centreImage: true);
@@ -27,7 +31,11 @@ namespace HandwrittenDigitRecognition
             var trainingData = ImageSample.LoadTrainingImages();   // 50,000 training images
             var testingData = ImageSample.LoadTestingImages();     // 10,000 testing images
 
-            await Task.Run(() => trainer.Train(trainingData, testingData, learningRate: .01, epochs: 10));
+            await Task.Run(() => trainer.Train(
+                trainingData,
+                testingData,
+                learningRate: .01,
+                epochs: numberOfEpochs));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Demonstration of a neural network using C#
 
-Taken from Joseph Albahari's LINQPad sample code "Samples for YOW/NDC 2018 - Writing a Neural Net from Scratch", and repackaged as a WPF application using Visual Studio 2017.
+Taken from Joseph Albahari's LINQPad sample code "Samples for YOW/NDC 2018 - Writing a Neural Net from Scratch", and repackaged as a Visual Studio 2017 solution.
 
 https://www.linqpad.net/RichClient/WritingNeuralNet.zip
+
+## Running it
+
+1. Load the solution in Visual Studio 2017
+a. Class library: .NET Framework 4.6.1
+b. UI: WPF App, .NET Framework 4.6.1
+2. Set `HandwrittenDigitRecognition` as the StartUp Project
+3. Press F5!
+
+## Playing around with it
+
+1. Open `Recognizer.cs` under `HandwrittenDigitRecognition`.
+2. Change `activator` to `new ReluActivator()` (line 13).
+3. Increase `numberOfNeuronsInHiddenLayer` to 30 (line 15).
+4. Increase `numberOfEpochs` to 10 (line 17).
+5. Add more hidden layers (line 23).
